d570b9e9bc0daf692aec0d2fe741592766fa4ab7
Gavin Niederman <gavinniederman@gmail.com>
2022-06-22 16:44:57 -0700

    Finish piece logic excluding En pessant, checkmates, checks, and castling

--- src/lib.rs
+++ src/lib.rs
@@ -17,25 +17,25 @@ mod test {
         assert_eq!(offset, -17)
     }
 
-    #[test]
-    fn move_off_board() {
-        let gamestate = GameState {
-            boardheight: 8,
-            boardwidth: 8,
-            board: Board {
-                pawns: vec![pieces::Pawn {
-                    pos: 0,
-                    moveoffsets: vec![math::calculate_move_offset(math::Vec2{x: 0, y: 1}),
-                                      math::calculate_move_offset(math::Vec2{x: -1, y: 1}),
-                                      math::calculate_move_offset(math::Vec2{x: 1, y: 1})],
-                    color: Color::White,
-                }],
-                rooks: vec![],
-                bishops: vec![],
-                knights: vec![],
-                queens: vec![],
-                kings: vec![],
-            },
-        };
-    }
+    // #[test]
+    // fn move_off_board() {
+    //     let gamestate = GameState {
+    //         boardheight: 8,
+    //         boardwidth: 8,
+    //         board: Board {
+    //             pawns: vec![pieces::Pawn {
+    //                 pos: 0,
+    //                 moveoffsets: vec![math::calculate_move_offset(math::Vec2{x: 0, y: 1}),
+    //                                   math::calculate_move_offset(math::Vec2{x: -1, y: 1}),
+    //                                   math::calculate_move_offset(math::Vec2{x: 1, y: 1})],
+    //                 color: Color::White,
+    //             }],
+    //             rooks: vec![],
+    //             bishops: vec![],
+    //             knights: vec![],
+    //             queens: vec![],
+    //             kings: vec![],
+    //         },
+    //     };
+    // }
 }
